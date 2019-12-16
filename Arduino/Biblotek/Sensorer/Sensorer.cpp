3e51bfd191f6c63efb47775a6e7eb02761dc0f78
Lucketyp <57945369+Lucketyp@users.noreply.github.com>
2019-12-16 14:38:13 +0100

    Add files via upload

--- Arduino/Biblotek/Sensorer/Sensorer.cpp
+++ Arduino/Biblotek/Sensorer/Sensorer.cpp
@@ -11,10 +11,10 @@ Sensorer::Sensorer(byte _sensors[3])
 void Sensorer::readSensors() 
 {
    for (int i = 0; i < sizeof(sensors); i+=1) {
-    volts[i] = analogRead(sensors[i]) * 0.0048828125; // VÃ¤rde frÃ¥n sensor * konstant
-    distance[i] = 13*pow(volts[i], -1); // utrÃ¤knat frÃ¥n indiern pÃ¥ youtube
+    volts[i] = analogRead(sensors[i]) * 0.0048828125; // Värde från sensor * konstant
+    distance[i] = 13*pow(volts[i], -1); // uträknat från indiern på youtube
       if (distance[i] < 30 && distance[i] != 0) {
-        inputDis[i] = 1 - distance[i]/30; //Ger vÃ¤rde mellan 1 och 0 
+        inputDis[i] = 1 - distance[i]/30; //Ger värde mellan 1 och 0 
       } else {
         inputDis[i] = 0;
       }
